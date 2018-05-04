f557cb2eefb0071b68d7a67da5b6f7d0c2769389
Xiyeon <Xiyeon@192.168.0.127>
2018-05-04 17:15:37 +0900

    main, 리뷰 dao, dto 수정

--- src/et/model/dto/ReviewDTO.java
+++ src/et/model/dto/ReviewDTO.java
@@ -1,5 +1,85 @@
 package et.model.dto;
 
 public class ReviewDTO {
+	private String reviewId; //후기 id
+	private String partId; //참여 id
+	private String memberId; //회원 id
+	private String resId; //식당 id                               
+	private String reviewTitle; //제목
+	private String reviewContents; //내용
+	private int reviewReadnum; //조회수
+	private String fileName; //첨부파일 이름
+	private int fileSize; //첨부파일 크기
+	private String reviewPw; //게시물 비밀번호
+	
+	public void ReviewDTO() {}
 
+	public ReviewDTO(String reviewId, String reviewTitle, String reviewContents, int reviewReadnum, String fileName,
+			int fileSize, String reviewPw) {
+		super();
+		this.reviewId = reviewId;
+		this.reviewTitle = reviewTitle;
+		this.reviewContents = reviewContents;
+		this.reviewReadnum = reviewReadnum;
+		this.fileName = fileName;
+		this.fileSize = fileSize;
+		this.reviewPw = reviewPw;
+	}
+
+	public String getReviewId() {
+		return reviewId;
+	}
+
+	public void setReviewId(String reviewId) {
+		this.reviewId = reviewId;
+	}
+
+	public String getReviewTitle() {
+		return reviewTitle;
+	}
+
+	public void setReviewTitle(String reviewTitle) {
+		this.reviewTitle = reviewTitle;
+	}
+
+	public String getReviewContents() {
+		return reviewContents;
+	}
+
+	public void setReviewContents(String reviewContents) {
+		this.reviewContents = reviewContents;
+	}
+
+	public int getReviewReadnum() {
+		return reviewReadnum;
+	}
+
+	public void setReviewReadnum(int reviewReadnum) {
+		this.reviewReadnum = reviewReadnum;
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public int getFileSize() {
+		return fileSize;
+	}
+
+	public void setFileSize(int fileSize) {
+		this.fileSize = fileSize;
+	}
+
+	public String getReviewPw() {
+		return reviewPw;
+	}
+
+	public void setReviewPw(String reviewPw) {
+		this.reviewPw = reviewPw;
+	}
+	
 }
