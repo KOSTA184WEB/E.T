bca75fab21da272026ad4b9143574a2933bd40c7
LittleGiantBaek <horowitz@hanmail.net>
2018-05-04 15:14:17 +0900

    InsertMeetingAction 설계진행중 & actionMapping keyword 지정

---
+++ src/et/controller/InsertMeetingAction.java
@@ -0,0 +1,17 @@
+package et.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class InsertMeetingAction implements Action {
+
+	@Override
+	public ModelAndView execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
